91aa4a3ac32f03a433ca66a19e489048bc6ec2ce
Guiomar Almunia Herranz <galmuniaia@gmail.com>
2023-07-22 04:30:01 +0200

    antes de probar angular animations; por si se rompe todo

--- src/app/common-components/header/header.component.ts
+++ src/app/common-components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,9 +9,11 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent  implements OnInit {
   public selectedLanguage: string = 'en';
+  public segment: string = ''; 
 
   constructor(
-     private translate: TranslateService
+     private translate: TranslateService,
+     private router: Router
   ) { }
 
   ngOnInit() {}
@@ -18,6 +21,34 @@ export class HeaderComponent  implements OnInit {
 
   changeLanguage() {
     this.translate.use(this.selectedLanguage);
+  }
+
+
+  openSettings() {
+     
+  }
+
+  segmentChanged(event: any) {
+    // Here you can handle the segment change and navigate accordingly
+    switch (event.detail.value) {
+      case 'gallery':
+        // Handle the navigation for the gallery segment
+        console.log('Navigate to Gallery');
+        this.router.navigate(['gallery']);
+        break;
+      case 'shop':
+        // Handle the navigation for the shop segment
+        console.log('Navigate to Shop');
+        this.router.navigate(['shop']);
+        break;
+      case 'minigame':
+        // Handle the navigation for the minigame segment
+        console.log('Navigate to Minigame');
+        this.router.navigate(['game']);
+        break;
+      default:
+        break;
+    }
   }
 
 }
